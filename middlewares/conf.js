853891641664145d5b284fdd1705a202c191b8e5
Jupiter-Lee <450663973@qq.com>
2017-05-18 17:46:18 +0800

    第三方登录测试

--- middlewares/conf.js
+++ middlewares/conf.js
@@ -1,7 +1,7 @@
 var config = require('../config');
 
 exports.github = function (req, res, next) {
-  if (config.GITHUB_OAUTH.clientID === 'your GITHUB_CLIENT_ID') {
+  if (config.GITHUB_OAUTH.clientID === '381f224c20fee86153ca16278bf01063e84edf5c') {
     return res.send('call the admin to set github oauth.');
   }
   next();
